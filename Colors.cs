0559ce6756eb3de11b137c6a1cad69ad53e10f47
Phil H <ph-hawkins@outlook.com>
2017-06-08 19:17:29 -0400

    Work

--- Colors.cs
+++ Colors.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using Shell.NET;
 
 namespace ConsoleColors
 {
@@ -8,66 +9,34 @@ namespace ConsoleColors
     {
         private static Process _shell;
         private static bool isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
-        public const string Bold = "$(tput bold)";
-        public const string Reset = "$(tput sgr0)";
-        public const string Black = "$(tput setaf 0)";
-        public const string Red = "$(tput setaf 1)";
-        public const string Green = "$(tput setaf 2)";
-        public const string Yellow = "$(tput setaf 3)";
-        public const string Blue = "$(tput setaf 4)";
-        public const string Magenta = "$(tput setaf 5)";
-        public const string Cyan = "$(tput setaf 6)";
-        public const string White = "$(tput setaf 7)";
+        private static bool isMac = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+        public static string Bold { get; } = "$(tput bold)";
+        public static string Reset { get; } = "$(tput sgr0)";
+        public static string Black { get; } = "$(tput setaf 0)";
+        public static string Red { get; } = "$(tput setaf 1)";
+        public static string Green { get; } = "$(tput setaf 2)";
+        public static string Yellow { get; } = "$(tput setaf 3)";
+        public static string Blue { get; } = "$(tput setaf 4)";
+        public static string Magenta { get; } = "$(tput setaf 5)";
+        public static string Cyan { get; } = "$(tput setaf 6)";
+        public static string White { get; } = "$(tput setaf 7)";
+        private static Bash _bash = new Bash();
 
         public static void Write(string output)
         {
-            if (isLinux)
-            {
-                StartShell(NewProcess(output, "-n"));
-                _shell.WaitForExit();
-                CloseShell();
-            }
+            if (isLinux || isMac)
+                _bash.nEcho(output);
             else
-            {
                 Console.Write(output);
-            }
         }
 
         public static void WriteLine(string output)
         {
-            if (isLinux)
-            {
-                StartShell(NewProcess(output));
-                _shell.WaitForExit();
-                CloseShell();
-            }
+            if (isLinux || isMac)
+                _bash.Echo(output);
             else
-            {
-                Console.WriteLine(output);
-            }
-        }
-
-        private static void StartShell(ProcessStartInfo info)
-        {
-            _shell = new Process { StartInfo = info };
-            _shell.Start();
-        }
-
-        private static void CloseShell()
-        {
-            _shell.Close();
-        }
-
-        private static ProcessStartInfo NewProcess(string output, string flag = null)
-        {
-            return new ProcessStartInfo
-            {
-                FileName = $"echo {flag} {output}",
-                RedirectStandardInput = false,
-                UseShellExecute = true,
-                CreateNoWindow = true,
-                ErrorDialog = false
-            };
+                Console.Write(output);
         }
 
         public static void SayHello()
