e330aabd6ced8544b1e808d93ee2771ec6ce3f70
Phil H <ph-hawkins@outlook.com>
2017-06-08 19:48:12 -0400

    Reorganize

---
+++ TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Diagnostics;
+using ConsoleColors;
+
+namespace TestApp
+{
+    class Program
+    {
+        static string version = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+
+        static void Main(string[] args)
+        {
+            Clr.WriteLine($"This library uses {Clr.Cyan}Shell.NET{Clr.Reset}!");
+        }
+    }
+}
