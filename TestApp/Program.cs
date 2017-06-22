b5d20193ec0bb3f85814e4267f21d6667acf6e30
Phil H <ph-hawkins@outlook.com>
2017-06-22 01:01:43 -0400

    New updates
     - Updated Shell.NET version
     - Added Clr.SetColor() methods
        - Example: Clr.SetCyan();

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -10,7 +10,8 @@ namespace TestApp
         static void Main(string[] args)
         {
             Clr.SayHello();
-            Clr.WriteLine($"This library uses {Clr.Bold}{Clr.Cyan}Shell.NET{Clr.Reset}!");
+            Clr.SetCyan();
+            Clr.WriteLine($"This library uses {Clr.Bold}Shell.NET{Clr.Reset}!");
             Clr.WriteLine(string.Format("{0}{4}C#{5} in {2}{4}Linux{5} is pretty {3}{4}cool!{5}",
                 Clr.Magenta,
                 Clr.White,
